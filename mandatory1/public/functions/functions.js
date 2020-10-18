f68b87a17627b24b6c7bc92a23cf24dd49e13f9d
Loctarjay <Jannicklp89@gmail.com>
2020-10-18 22:45:24 +0200

    Last Commit

--- mandatory1/public/functions/functions.js
+++ mandatory1/public/functions/functions.js
@@ -1,17 +1,24 @@
 console.log("Testing Function Information");
 
-
-
-
-
-function chosenName(){
-    // Enter 
+// Function declaration
+function chosenName( parameter ){
+    // Enter Code here
 }
 
 
+// Anonymous Function
+const func = function ( paramater ){
+    // Enter code here
+}
+func();
 
 
-
-
-
-
+// Callback function
+function firstFunction( number, parameter ){
+    // Enter code here
+    parameter(number)
+}
+function secondFunction( number ) {
+    // something with the number
+}
+firstFunction(2, secondFunction);
--- mandatory1/public/snippets/snippets.js
+++
@@ -1,1 +0,0 @@
-console.log("Testing Code Snippet page");
